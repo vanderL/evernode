ac796242b983156eb60ee3a39df023b252c8acfa
Vander Lima <vnd.vander@gmail.com>
2020-11-11 17:57:09 -0300

    Construct base API and Created strutucte of data

---
+++ backend/evernode_api/app/models/note.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose')
+
+let noteSchema = new mongoose.Schema({
+    title: String,
+    body: String,
+    created_at: { type: Date, default: Date.now},
+    updated_at: { type: Date, default: Date.now},
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+
+})
---
+++ backend/evernode_api/config/database.js
@@ -0,0 +1,10 @@
+const  mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost/javascriptNote', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}
+).then(() => console.log('connection succesful'))
+  .catch((err) => console.error('Deu erro: '+err));
