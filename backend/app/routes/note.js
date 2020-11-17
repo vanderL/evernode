f3be30befcbdc591859a67b849171daa797f3366
Vander Lima <vnd.vander@gmail.com>
2020-11-17 11:45:28 -0300

    get a note with authetication token

--- backend/app/routes/note.js
+++ backend/app/routes/note.js
@@ -16,4 +16,28 @@ router.post('/', withAuth, async function(req, res) {
     }
 })
 
+router.get('/:id', withAuth, async function(req, res){
+    
+    try {
+        const { id } = req.params
+        let note =  await Note.findById(id)
+        if(isOwner(req.user,note)){
+            res.json(note)
+        } else {
+            res.status(403).json({error: "permission denied"})
+        }
+    } catch (error) {
+        res.status(500).json({error: "Problem to get a note"})
+    }
+})
+
+const isOwner = (user, note) => {
+   if(JSON.stringify(user._id) == JSON.stringify(note.author._id)){
+    return true 
+    
+   }else {
+       return false;
+   }
+}
+
 module.exports = router
