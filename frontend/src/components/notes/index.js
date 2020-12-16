edc2f03a86754d9a9fa6ac54c81ba6111d31e47d
Vander Lima <vnd.vander@gmail.com>
2020-12-16 16:28:57 -0300

    corrections Create Notes and ListNotes

--- frontend/src/components/notes/index.js
+++ frontend/src/components/notes/index.js
@@ -1,64 +1,87 @@
-import React, { Fragment, useEffect, useState} from 'react'
-import List from "../notes/list"
-import NoteService from '../../services/note'
-import {Column, Button} from "rbx"
-import '../../styles/notes.scss'
+import React, { Fragment, useEffect, useState } from 'react';
+import { Column, Button } from "rbx"
+import "../../styles/notes.scss"
 import { push as Menu } from 'react-burger-menu'
+import List from "../notes/list"
+import NotesService from '../../services/note'
 
 function Notes(props) {
-    const [notes, setNotes] = useState([])
-    const [current_note, setCurrentNote] = useState({title: "", body: "", id: ""})
-    
-    useEffect(() => {
-        fetchNotes();
-      }, []);
-    
-      async function fetchNotes(){
-          const response = await NoteService.index()
-          if (response.data.lenght >= 1) {
-              setNotes(response.data.reverse())
-              setCurrentNote(response.data[0])
-          }
-      }
+  const [notes, setNotes] = useState([])
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
 
-      const selectNote = (id) => {
-          const note = note.find((note) => {
-              return note._id == id;
-          })
-          setCurrentNote(note)
-      }
+  async function fetchNotes() {
+    const response = await NotesService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
+    } else {
+      setNotes([])
+    }
+  }
 
-      const createNote = async (params) => {
-          const note =  await NoteService.create()
-          fetchNotes()
-      }
+  const createNote = async () => {
+    await NotesService.create()
+    fetchNotes()
+  }
 
-    return(
-        <Fragment>
-            <Column.Group className="notes" id="notes">
-                <Menu
-                    pageWrapId={"notes-editor"}
-                    isOpen={props.isOpen}
-                    onStateChange={(state) => props.setIsOpen(state.isOpen)}
-                    disableAutoFocus
-                    outerContainer={"notes"}
-                    customBurgerIcon={false}
-                    customCrossIcon={false}
-                >
-                    <List 
-                        notes={notes}
-                        selectNote={selectNote}
-                        createNote={createNote}
-                        current_note={current_note}/>
-                </Menu>
+  const deleteNote = async (note) => {
+    await NotesService.delete(note._id)
+    fetchNotes()
+  }
 
-                <Column size={12} className="notes-editor" id="notes-editor">
-                    Editor...
-                </Column>
+    const updateNote = async (oldNote, params) => {
+        const updatedNote = await NotesService.update(oldNote._id, params)
+        const index = notes.indexOf(oldNote)
+        const newNotes = notes
+        newNotes[index] = updatedNote.data
+        setNotes(newNotes)
+        setCurrentNote(updatedNote.data)
+    }
 
-            </Column.Group>
-        </Fragment>
-    )
+    const searchNotes = async (query) => {
+        const response = await NotesService.search(query)
+    setNotes(response.data)
+    }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id == id
+    })
+    setCurrentNote(note)
+  }
+
+  useEffect(() => {
+    fetchNotes()
+  }, [])
+
+  return (
+    <Fragment>
+      <Column.Group className="notes" id="notes">
+        <Menu
+          pageWrapId={"notes-editor"}
+          isOpen={props.isOpen}
+          onStateChange={(state) => props.setIsOpen(state.isOpen)}
+          disableAutoFocus
+          outerContainerId={"notes"}
+          customBurgerIcon={false}
+          customCrossIcon={false}
+        >
+          <Column.Group>
+           
+          </Column.Group>
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note} 
+            deleteNote={deleteNote}
+            createNote={createNote}/>
+        </Menu>
+
+
+        
+      </Column.Group>
+    </Fragment>
+  )
 }
 
 export default Notes
--- frontend/src/services/note.js
+++ frontend/src/services/note.js
@@ -2,10 +2,11 @@ import Api from "./api"
 
 const NoteService = {
     create: () => Api.post('/notes', {'title': 'Nova nota', 'body': 'Nova nota...'}, {
-        headers: {'x-access-token': localStorage.getItem('token')}
-      }),
-    index: () => Api.get("/notes", {
-        headers: {'x-access-token': localStorage.getItem('token')}
+        headers: {'x-acess-token': localStorage.getItem('token')}
+    }),
+
+    index: () => Api.get('/notes', {
+        headers: {'x-acess-token': localStorage.getItem('token')}
     })
 }
 
