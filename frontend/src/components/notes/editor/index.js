9631f9a70432a9f4002ec9997afb18859c3374c3
Vander Lima <vnd.vander@gmail.com>
2020-12-17 15:28:28 -0300

    Update note

--- frontend/src/components/notes/editor/index.js
+++ frontend/src/components/notes/editor/index.js
@@ -5,10 +5,25 @@ import 'react-quill/dist/quill.snow.css'
 
 function Editor(props) {
     const [currentContent, setCurrentContent] = useState('')
+    
+    const [timer, setTimer] = useState(null)
+
+    const  updateNote = (content) => {
+        const title = content.replace(/(<([^>]+)>)/ig, "").slice(0, 30)
+        props.updateNote(props.note, {'title': title, 'body': content})
+    }
 
     useEffect(() => {
         setCurrentContent(props.note.body)
     }, [props.note])
+
+    const handleChange = (content, delta, source ) => {
+        clearTimeout(timer)
+        if(source == 'user'){
+            setCurrentContent(content)
+            setTimer(setTimeout(() => updateNote(content), 2000))
+        }
+    }
 
     const modules = {
         toolbar: [
@@ -23,7 +38,7 @@ function Editor(props) {
 
     return (
         <Fragment>
-            <ReactQuill value={currentContent} modules={modules} />
+            <ReactQuill value={currentContent} onChange={handleChange} modules={modules} />
         </Fragment>
     )
 }
